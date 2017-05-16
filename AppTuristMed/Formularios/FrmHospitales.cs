7612cbe766dd67483f0ce84419df1df5fd350bd7
JuanDGiraldoM <judagima@gmail.com>
2017-05-16 16:32:48 -0500

    Formularios agregados

--- AppTuristMed/Formularios/FrmHospitales.cs
+++ AppTuristMed/Formularios/FrmHospitales.cs
@@ -12,9 +12,30 @@ namespace AppTuristMed.Formularios
 {
     public partial class FrmHospitales : Form
     {
-        public FrmHospitales()
+        private FrmMain main;
+        
+        public FrmHospitales(FrmMain main, string conexion)
         {
             InitializeComponent();
+            this.main = main;
+            oleDbConnection.ConnectionString = conexion;
+        }
+
+        private void FrmHospitales_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Hide();
+            main.Show();
+        }
+
+        private void FrmHospitales_Load(object sender, EventArgs e)
+        {
+            oleDbConnection.Open();
+            oleDbDataAdapter.SelectCommand.CommandText = "select Municipio, Nombre_Sede, Direccion, Telefono, Email, from Hospitales";
+            oleDbDataAdapter.SelectCommand.Connection = oleDbConnection;
+            DataTable datos = new DataTable();
+            oleDbDataAdapter.Fill(datos);
+            table.DataSource = datos;
+            oleDbConnection.Close();
         }
     }
 }
